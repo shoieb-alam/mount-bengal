9064812e36fc8096f78d08d5f8c875bc75316260
shoieb-alam <86704871+shoieb-alam@users.noreply.github.com>
2021-10-18 10:10:23 +0600

    components added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Page/Shared/Header/Header';
+import Footer from './components/Page/Shared/Footer/Footer';
+import Home from './components/Page/Home/Home/Home';
+import Login from './components/Page/Login/Login';
+import Services from './components/Page/Home/Services/Services';
+import NotFound from './components/Page/NotFound/NotFound';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/login">
+            <Login></Login>
+          </Route>
+          <Route path="/services">
+            <Services></Services>
+          </Route>
+          <Route path="">
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+        <Footer></Footer>
+      </Router>
     </div>
   );
 }
