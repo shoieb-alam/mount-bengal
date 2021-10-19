a86a3cafda9f516f797cd18c2faffd3f3366ef97
shoieb-alam <86704871+shoieb-alam@users.noreply.github.com>
2021-10-19 21:04:34 +0600

    button added

--- src/Page/Home/Service/Service.js
+++ src/Page/Home/Service/Service.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Col } from 'react-bootstrap';
+import { Button, Card, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './Service.css';
 
@@ -12,12 +12,13 @@ const Service = ({ service }) => {
             {/* showing service data in home page */}
             <Col>
                 <Card as={Link} to={`/services/${id}`} className="card text-decoration-none rounded-3 py-2 h-100">
-                    <Card.Img variant="top" src={img} className="w-75 mx-auto" />
+                    <Card.Img variant="top" src={img} className="w-75 rounded-3 mx-auto" />
                     <Card.Body>
                         <Card.Title>{name}</Card.Title>
                         <Card.Text>
                             <h6> {title} </h6>
                         </Card.Text>
+                        <Button variant="dark" className="btn-outline-light">View Details</Button>
                     </Card.Body>
                 </Card>
             </Col>
